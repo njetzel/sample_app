d22aee77725f2259251ffbafb3f01649aa3a5192
Nick Etzel <njetzel2@gmail.com>
2014-04-01 16:07:55 -0500

    Finish exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -43,12 +43,7 @@ describe "User pages" do
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+      before { valid_signup_info }
       
       describe "after saving the user" do
         before { click_button submit }
@@ -56,7 +51,7 @@ describe "User pages" do
         
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_success_message('Welcome') }
       end
       
       it "should create a user" do
